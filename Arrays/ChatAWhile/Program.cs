0ac64392487d20f1039edd2052e3ef8d6acb07d2
Peter Mbanda <pmbandaft@gmail.com>
2016-11-08 23:12:21 -0600

    Documentation update

--- Arrays/ChatAWhile/Program.cs
+++ Arrays/ChatAWhile/Program.cs
@@ -11,46 +11,49 @@ namespace ChatAWhile
     {
         static void Main(string[] args)
         {
+            // declare class variables
             double total;
             bool exists = false;
 
             //array of area codes 
             Int32[] areaCode = { 262, 414, 608, 715, 815, 920 };
 
-            //rate of calls array 
+            //  rate of calls array 
             double[] per_minute_rate = { 0.07, 0.10, 0.05, 0.16, 0.24, 0.14 };
 
-            //prompt the user for the area code
+            //  prompt the user for the area code
             Write("Enter area code: ");
             int code = Convert.ToInt32(ReadLine());
+
             WriteLine();
 
-            //iterate through the loop to determine if the zip code exists
-            for (int i = 0; i < areaCode.Length ; ++i)
+            //  iterate through the loop to determine if the zip code exists
+            for (int i = 0; i < areaCode.Length; ++i)
             {
                 if (code == areaCode[i])
                 {
-                    //prompt user for tome entry 
+                    // prompt user for time entry 
                     Write("Price per minute for the call is >> " + per_minute_rate[i]);
                     WriteLine();
-                    WriteLine();//empty white space 
 
-                    //prompt user for the length of time the call made in minutes
-                    Write("Enter the time in minutes for the call >> ");
+                    // prompt user for the length of time the call made in minutes
+                    Write("Enter the time in minutes for the call: >> ");
                     double time = Convert.ToDouble(ReadLine());
 
-                    total = time * per_minute_rate[i];//multiply the duration of call and price per minute 
-                    WriteLine();
+                    // multiply the duration of call and price per minute 
+                    total = time * per_minute_rate[i];
 
+                    // assign if zip code is accessed
                     WriteLine("The total cost for the call is " + total.ToString("C2"));
-                    exists = true;//assign if zip code is accessed
+                    exists = true;
                 }
             }
 
-            //if the zip code does NOT exist display the message below
+            // if the zip code does NOT exist display the message below
             if (!exists)
                 WriteLine("The zip code does NOT exist ");
             ReadLine();
+
         }
     }
 }
