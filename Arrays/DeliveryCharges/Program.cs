120bae612dadcdf12d6950e609b7699183d1cf6d
Peter Mbanda <pmbandaft@gmail.com>
2016-11-08 23:41:55 -0600

    Correct code comment

--- Arrays/DeliveryCharges/Program.cs
+++ Arrays/DeliveryCharges/Program.cs
@@ -11,18 +11,20 @@ namespace DeliveryCharges
     {
         static void Main(string[] args)
         {
+            // class variable
             bool exist = false;
-            //initialize the zip codes in the array
+
+            // initialize the zip codes in the array
             Int32[] zipCodes = new Int32[] { 73120, 74142, 75642, 76534, 72341, 75643, 76523, 75684, 75553, 76635 };
 
-            //initialize the prices in the delivery areas
+            // initialize the prices in the delivery areas
             double[] prices = new double[] { 100.25, 22.56, 800.25, 96.45, 122.32, 56.32, 456.25, 78.25, 965.25, 10.00 };
 
-            //prompt the user to enter zip code
+            // prompt the user to enter zip code
             Write("Enter zip code: ");
             int zipEntry = Convert.ToInt32(ReadLine());
 
-            //iterate through loop to verify if a zip code matches 
+            // iterate through loop to verify if a zip code matches 
             for (int i = 0; i < zipCodes.Length; ++i)
             {
                 if (zipEntry == zipCodes[i])
@@ -34,6 +36,7 @@ namespace DeliveryCharges
                 }
                 
             }
+
             //if the zip code does not match display the message
             if(!exist)
                 WriteLine("Company does NOT deliver to zipcode ");
