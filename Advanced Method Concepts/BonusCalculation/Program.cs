8ec98a93a265b4a0c865bfa79ee15c9d66146d28
Peter Mbanda <pmbandaft@gmail.com>
2016-11-08 19:45:23 -0600

    Present data in currency format

--- Advanced Method Concepts/BonusCalculation/Program.cs
+++ Advanced Method Concepts/BonusCalculation/Program.cs
@@ -13,16 +13,17 @@ namespace BonusCalculation
     {
         static void Main(string[] args)
         {
-            //declare variables to be assigned in the methods as output reference
+            //  declare variables to be assigned in the methods as output reference
             double salary, bonus;
             int bonusInt;
-            //invoke method and pass double as arguments
+
+            //  invoke method and pass two double values as arguments
             Console.WriteLine("Arguments passed as double: ");
             SalaryBonusCalc(out salary, out bonus);
 
             Console.WriteLine();
 
-            //invoke method and pass double and integer as arguments
+            //  invoke method and pass double and integer as arguments
             Console.WriteLine("Arguments passed as double and integer: ");
             SalaryBonusCalc(out salary, out bonusInt);
 
@@ -31,34 +32,38 @@ namespace BonusCalculation
 
         static void SalaryBonusCalc(out double salary, out double bonus)
         {
-            //assign values to the variables 
+            // declare and assign values to the variables 
+            double total;
             salary = 600.00;
             bonus = 0.10;
-            //declare the total variable
-            double total;
-            bonus *= 100;//convert bonus into a percentage 
-            total = bonus + salary;//add the bonus to the salary and assign the value to the total variable
 
-            //display the bonus salary and the total amount
-            Console.WriteLine("Salary {0} ", salary);
-            Console.WriteLine("Bonus {0} ", bonus);
-            Console.WriteLine("Total {0} ", total.ToString("F"));
+            // convert bonus into a percentage 
+            bonus *= 100;
+
+            // add the bonus to the salary and assign the value to the total variable
+            total = bonus + salary;
+
+            // display the bonus salary and the total amount
+            Console.WriteLine("Salary {0} ", salary.ToString("C"));
+            Console.WriteLine("Bonus {0} ", bonus.ToString("C"));
+            Console.WriteLine("Total {0} ", total.ToString("C2"));
 
         }
 
         static void SalaryBonusCalc(out double salary, out int bonusInt)
         {
-            //assign values to the variables 
+            // declare and assign values to the variables 
+            double total;
             salary = 600.00;
             bonusInt = 50;
-            //declare the total variable
-            double total;
-            total = bonusInt + salary;//add the bonus to the salary and assign the value to the total variable
 
-            //display the bonus salary and the total amount
-            Console.WriteLine("Salary {0} ", salary);
-            Console.WriteLine("Bonus {0} ", bonusInt);
-            Console.WriteLine("Total {0} ", total.ToString("F"));
+            // add the bonus to the salary and assign the value to the total variable
+            total = bonusInt + salary;
+
+            // display the bonus salary and the total amount
+            Console.WriteLine("Salary {0} ", salary.ToString("C"));
+            Console.WriteLine("Bonus {0} ", bonusInt.ToString("C"));
+            Console.WriteLine("Total {0} ", total.ToString("C2"));
         }
     }
 }
