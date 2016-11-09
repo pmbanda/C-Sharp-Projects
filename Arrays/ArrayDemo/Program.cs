c88749b1a074fd6c6d5859b4f3a9056b9a5a6940
Peter Mbanda <pmbandaft@gmail.com>
2016-11-08 22:04:01 -0600

    Dilay non zero values on array display

--- Arrays/ArrayDemo/Program.cs
+++ Arrays/ArrayDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿//Author:   Peter Eugene Mbanda
 //Date:     1/29/2016
 //Purpose:  Array Demo Application
+
 using System;
 using static System.Console;
 
@@ -10,34 +11,37 @@ namespace ArrayDemo
     {
         static void Main(string[] args)
         {
-            //sentinel value declaration to terminate entry 
+            // sentinel value declaration to terminate entry 
             const int sentinel_value = 999;
             int option;
 
-            //total size of array
+            // total size of array
             const int size = 8;
-            int[] array = new int[size];
+            int[] arrayVal = new int[size];
 
-            //iterate through the array to enter the array contents
-            for (int i = 0; i < array.Length; ++i)
+            // iterate through the array to enter the array contents
+            for (int i = 0; i < arrayVal.Length; ++i)
             {
+                // convert the array value to an integer
                 Write("Enter a value in the array >> ");
-                array[i] = Convert.ToInt32(ReadLine());//convert the array value to an integer
+                arrayVal[i] = Convert.ToInt32(ReadLine());
 
-                if (array[i] != sentinel_value)
+                if (arrayVal[i] != sentinel_value)
                     continue;
                 else
                 {
-                    array[i] = 0;
-                    break;//break out of array if the sentinel value has been entered
+                    // break out of array if the sentinel value has been entered
+                    arrayVal[i] = 0;
+                    break;
                 }
             }
             WriteLine();
 
-            //user prompt for options 
+            // user prompt for options 
             WriteLine("Enter 1 to (view list in order from first to last )");
             WriteLine("Enter 2 to (view list in order from last to first )");
-            WriteLine("Enter 3 to (chose a specific position to view )");
+            WriteLine("Enter 3 to (chose a specific position to view )\n");
+
             option = Convert.ToInt32(ReadLine());//enter the option chosen by the user
 
             WriteLine();
@@ -45,39 +49,68 @@ namespace ArrayDemo
             switch (option)
             {
                 case 1:
-                    //iterate through the list to
-                    //view array list in order from the first to the last
-                    WriteLine("View array contents from the first to the last ");
-                    for (int i = 0; i < array.Length; ++i)
-                        WriteLine(array[i]);
+                    firstToLast(arrayVal);
                     break;
-
                 case 2:
-                    //iterate through the list to
-                    //view array list in order from the last to the first
-                    Array.Reverse(array);
-                    WriteLine("View array contents from the last to the first ");
-                    for (int i = 0; i < array.Length; ++i)
-                        WriteLine(array[i]);
+                    lastToFirst(arrayVal);
                     break;
-
                 case 3:
-                    //prompt user for index input
-                    WriteLine("Enter the index you want to view between 0 and " + (array.Length - 1));
-                    int num = Convert.ToInt32(ReadLine());
-
-                    for (int i = 0; i < array.Length; ++i) { }
-                 
-                    //display index if the array falls within range 
-                    if (num <= (array.Length - 1))
-                        WriteLine("Content at position " + num + " is " + array[num]);
-                    else
-                        WriteLine("Index you entered does not exist ");//index out of range 
+                    byIndex(arrayVal);
                     break;
-                    //default:
             }
 
             ReadLine();
+        }
+
+        static void firstToLast(params int[] value)
+        {
+            /*
+                iterate through the list to
+                view array list in order from the last to the first
+            */
+            Array.Reverse(value);
+            WriteLine("View array contents from the last to the first ");
+            for (int i = 0; i < value.Length; ++i)
+            {
+                if (value[i] == 0)
+                    return;
+                WriteLine(value[i]);
+            }
+               
+        }
+
+        static void lastToFirst(params int[] value)
+        {
+            /*
+                iterate through the list to
+                view array list in order from the first to the last
+            */
+            WriteLine("View array contents from the first to the last ");
+            for (int i = 0; i < value.Length; ++i)
+            {
+                if (value[i] == 0)
+                    return;
+                WriteLine(value[i]);
+            }
+        }
+
+        static void byIndex(params int[] value)
+        {
+            //  prompt user for index input
+            WriteLine("Enter the index you want to view between 0 and " + (value.Length - 1));
+            int num = Convert.ToInt32(ReadLine());
+
+            if (num <= (value.Length - 1))
+            {
+                // display index if the array falls within range 
+                WriteLine("Content at position " + num + " is " + value[num]);
+            }
+            else
+            {
+                // index out of range
+                WriteLine("Index you entered does not exist ");
+            }
+            
         }
     }
 }
